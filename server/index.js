a9515f4c53c215f9736d4fe72f251965a0efd463
esme <23175119+esme@users.noreply.github.com>
2019-08-17 22:54:53 -0700

    set process env port

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const Stocks = require('stocks.js');
 // const db = require('./db');
 
 const app = express();
-const port = 3000 || process.env.PORT;
+const port = process.env.PORT || 3000;
 const stocks = new Stocks('PVY8MCGPGIBP69X4');
 
 // var schema = buildSchema(`
